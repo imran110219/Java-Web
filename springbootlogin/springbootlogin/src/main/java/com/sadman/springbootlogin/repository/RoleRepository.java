a6799f75116adeaa04587119c020cca8ca06cb3c
imran110219 <imran110219@gmail.com>
2020-04-30 12:28:13 +0600

    Spring Boot Login

---
+++ springbootlogin/springbootlogin/src/main/java/com/sadman/springbootlogin/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.sadman.springbootlogin.repository;
+
+import com.sadman.springbootlogin.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+}
+

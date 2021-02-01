38a6866ee08336c5a6505a13dda41ffc456f5f3e
Sadman <imran110219@gmail.com>
2021-02-01 17:02:48 +0600

    login module

---
+++ springbootlogin/src/main/java/com/sadman/springbootlogin/controller/api/LoginAPIController.java
@@ -0,0 +1,16 @@
+package com.sadman.springbootlogin.controller.api;
+
+import com.sadman.springbootlogin.model.User;
+import com.sadman.springbootlogin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+public class LoginAPIController {
+
+}
--- springbootlogin/src/main/java/com/sadman/springbootlogin/service/UserService.java
+++ springbootlogin/src/main/java/com/sadman/springbootlogin/service/UserService.java
@@ -7,9 +7,12 @@ import com.sadman.springbootlogin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -43,4 +46,7 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
